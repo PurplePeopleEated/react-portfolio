5b1bdf9de1bbd11b7bea23cb639f50f4e12e3960
Karra <karra.maynard@gmail.com>
2024-02-11 02:06:14 -0500

    create header with nav

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navigation from "./Navigation";
+
+function Header() {
+  return (
+    <nav className="flex">
+      <Navigation />
+    </nav>
+  );
+}
+
+export default Header;
