e0fc12075ae8f50fe068d6555a2242a1141c9e66
Karra <karra.maynard@gmail.com>
2024-02-11 02:04:29 -0500

    import react router and create a browser router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from './App.jsx'
 import './styles/tailwind.css'
 
+// Import components
+import About from './pages/About.jsx';
+import Contact from './pages/Contact.jsx';
+import Projects from './pages/Projects.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <About />
+      },
+      {
+        path: '/contact',
+        element: <Contact />
+      },
+      {
+        path: '/projects',
+        element: <Projects />
+      }
+    ]
+  }
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
-)
+);
