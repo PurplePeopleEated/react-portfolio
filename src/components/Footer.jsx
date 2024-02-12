950ca0759c6ef7bf09feead92e5ee97eb07622e5
Karra <karra.maynard@gmail.com>
2024-02-11 23:10:24 -0500

    add footer content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,12 @@ import React from "react";
 
 function Footer() {
   return (
-    <div>foot</div>
+    <div className="fixed inset-x-0 bottom-0 py-5 text-center">
+      {/* GitHub Link */}
+      <p className="text-sm mt-2 opacity-50">
+        &copy; {new Date().getFullYear()} <a className="hover:underline" href="https://github.com/PurplePeopleEated">Karra Maynard</a>. All rights reserved.
+      </p>
+    </div>
   );
 }
 
