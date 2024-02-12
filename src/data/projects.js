cd52cdc4ba77e55c1037e0e1ce72038c975f6970
Karra <karra.maynard@gmail.com>
2024-02-11 23:41:09 -0500

    add image links

---
+++ src/data/projects.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    title: "Virtual Pet",
+    img: "https://placehold.co/200x150",
+    deployedLink: "https://petappthingy-aa69f1e65e58.herokuapp.com/",
+    repoLink: "https://github.com/PurplePeopleEated/virtual-pet"
+  },
+  {
+    title: "Weather Dashboard",
+    img: "/assets/weather-dashboard.png",
+    deployedLink: "https://purplepeopleeated.github.io/weather-dashboard/",
+    repoLink: "https://github.com/PurplePeopleEated/weather-dashboard"
+  },
+  {
+    title: "Note Taker",
+    img: "/assets/note-taker.png",
+    deployedLink: "https://aqueous-eyrie-32269-fe93029542a7.herokuapp.com/",
+    repoLink: "https://github.com/PurplePeopleEated/note-taker"
+  },
+  {
+    title: "Board Game Fanatics",
+    img: "/assets/board-game-fanatics.png",
+    deployedLink: "https://limitless-woodland-06528-9312ac9a848a.herokuapp.com/",
+    repoLink: "https://github.com/PurplePeopleEated/board-game-fanatics"
+  },
+  {
+    title: "J.A.T.E.",
+    img: "/assets/jate.png",
+    deployedLink: "https://protected-lake-03628-b74414467477.herokuapp.com/",
+    repoLink: "https://github.com/PurplePeopleEated/text-editor"
+  },
+  {
+    title: "Tech Blog",
+    img: "https://placehold.co/200x150",
+    deployedLink: "https://boiling-mesa-75936-2db64dd6b5b0.herokuapp.com/",
+    repoLink: "https://github.com/PurplePeopleEated/tech-blog"
+  }
+]
