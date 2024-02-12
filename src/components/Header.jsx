fcfa73947236aa23ecb9bb16c215ba71fcd17399
Karra <karra.maynard@gmail.com>
2024-02-11 23:10:11 -0500

    header nav and tailwind style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import Navigation from "./Navigation";
 
 function Header() {
   return (
-    <nav className="flex">
+    <nav className="flex flex-col md:flex-row items-center justify-between bg-purple-800 p-4 mb-10">
       <Navigation />
     </nav>
   );
