2b8adbf19dfd544e118ef6fb2ae88679e19532f3
Karra <karra.maynard@gmail.com>
2024-02-11 23:11:40 -0500

    all projects listed

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,10 +1,25 @@
+import projects from "../data/projects";
+import Project from "../components/OneProject";
+
 function Projects() {
   return (
-    <>
-      <h1>Projects</h1>
-      
-    </>
-  )
+    <div className="flex flex-col md:flex-row items-center justify-center">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {projects.map(project => {
+          return (
+            <Project
+              key={project.id}
+              img={project.img}
+              title={project.title}
+              description={project.description}
+              deployedLink={project.deployedLink}
+              repoLink={project.repoLink}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Projects;
